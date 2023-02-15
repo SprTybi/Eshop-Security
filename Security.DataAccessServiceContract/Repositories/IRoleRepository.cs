6b366cfa4be5692cf06939dab1028383afb66c2e
mohammad9092 <mr.math9092@gmail.com>
2023-02-15 17:32:27 +0330

    020

--- Security.DataAccessServiceContract/Repositories/IRoleRepository.cs
+++ Security.DataAccessServiceContract/Repositories/IRoleRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Security.DataAccessServiceContract.Repositories
 {
-    public interface IRoleRepository
+    public interface IRoleRepository : IBaseRepositorySearchable<Role, int, Role, UserListItem>
     {
     }
 }
